987c726db889cc763b83bcb3b8f209a636644fee
alfar@bk.ru <alfar@bk.ru@6e715db3-a653-0410-8ee7-a14b35f615ba>
2009-01-14 16:06:37 +0000

    Added Event especially for chathist - fixed bug for combination forbid+chathist. Modified Kick - now it is possible to use it in this way Kick <nick> <message> Message will be sent to PM

--- plugins/chathist.py
+++ plugins/chathist.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
 # --- Chathist plugin.
 #
@@ -11,29 +11,23 @@ import yaml
 import time
 
 class chathist_plugin(plugin.plugin):
-        
-    def __init__(self,hub):
-          super(chathist_plugin,self).__init__(hub)
+		def __init__(self,hub):
+				super(chathist_plugin,self).__init__(hub)
+				if 'chathist' not in self.hub.settings or self.hub.settings['chathist']==[]:
+					self.hub.settings['chathist']={'lines': 20}
+				if 'chathist.db' not in self.hub.settings:
+					self.hub.settings['chathist.db'] = []
+				self.chlog=self.hub.settings['chathist.db']
+				self.settings=self.hub.settings['chathist']
+				self.slots['onConnected']=self.onConnected
+				self.slots['ChatHistEvent']=self.ChatHistEvent
 
-          if 'chathist' not in self.hub.settings or self.hub.settings['chathist']==[]:
-		 self.hub.settings['chathist']={'lines': 20}
-	  if 'chathist.db' not in self.hub.settings:
-		 self.hub.settings['chathist.db'] = []
+		def onConnected(self,user):
+			self.hub.send_to_nick( user.nick, self.hub._( ' ---- Last history ---- \n%s' ) % '\n'.join( self.chlog ) )
+			return True
 
-          self.chlog=self.hub.settings['chathist.db']
-	  self.settings=self.hub.settings['chathist']
-
-          self.slots['onConnected']=self.onConnected
-          self.slots['onMainChatMsg']=self.onMainChatMsg
-		
-    def onConnected(self,user):
-	    self.hub.send_to_nick( user.nick, self.hub._( ' ---- Last history ---- \n%s' ) % '\n'.join( self.chlog ) )
-	    return True
-          
-    def onMainChatMsg(self, from_nick, message): 
-
-	self.chlog.append('[%s] <%s> %s' % ( time.strftime( '%x %X' ), from_nick, message ) )
-	if len( self.chlog ) > self.settings['lines']:
-		self.chlog.pop(0)
-
-        return True    
+		def ChatHistEvent(self, from_nick, message): 
+			self.chlog.append('[%s] <%s> %s' % ( time.strftime( '%x %X' ), from_nick, message ) )
+			if len( self.chlog ) > self.settings['lines']:
+				self.chlog.pop(0)
+			return True    
